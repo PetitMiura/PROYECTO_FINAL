5c595914c7073b18cf631473ded7889724ec275b
Alejandro <petitmiura@gmail.com>
2023-07-07 22:58:18 +0200

    creada las rutas en routes de inicio, compra con su html, status esta creada sin procesar

--- miCartera/routes.py
+++ miCartera/routes.py
@@ -4,3 +4,12 @@ from flask import render_template, request, redirect, flash, url_for
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/compra")
+def compra():
+    return render_template("compra.html")
+
+@app.route("/status")
+def status():
+    return render_template("status.html")
